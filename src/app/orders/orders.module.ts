8f85ab433d223f8ddd7682f99e03027d5876f88a
howserss <howserss@gmail.com>
2020-10-14 11:50:40 -0400

    new CKEditor changes.

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
 
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
 @NgModule({
   declarations: [OrderListComponent],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    CKEditorModule
+    
   ]
 })
 export class OrdersModule { }
